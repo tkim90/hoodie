e5b28e5c309e7b1e4ddb460d211e038fdaec2ea0
TK <taehnkim@gmail.com>
2019-10-21 01:10:24 -0700

    Refactored input marker handlers

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import MapGL, { Marker } from 'react-map-gl';
 import config from '../mapboxConfig.js';
+import $ from 'jquery';
+import styled from 'styled-components';
+
+const InputElement = styled.input`
+  font-family: Helveitca Neue, sans-serif;
+  font-size: 30px;
+  border: 3px solid black;
+  z-index: 70;
+  position: absolute;
+  opacity: 1.0 !important;
+  border-radius: 25px;
+`;
 
 class Map extends Component {
   constructor(props) {
@@ -16,23 +28,46 @@ class Map extends Component {
         width: window.innerWidth,
         height: window.innerHeight,
       },
-      markers: []
+      markers: [],
+      textValues: [],
+      inputForm: [],
+      currCoords: []
     };
     this.addMarker = this.addMarker.bind(this);
+    this.appendInput = this.appendInput.bind(this);
+    this.onChangeInputHandler = this.onChangeInputHandler.bind(this);
+    this.removeInput = this.removeInput.bind(this);
   }
 
-  addMarker({lngLat: [longitude, latitude]}) {
-    // get long lat
-    // push to markers state array
+  appendInput({lngLat: [longitude, latitude]}) {
+    const newInputForm = this.state.inputForm;
+    newInputForm.push('input-form');
+    this.setState({ 
+      inputForm: newInputForm,
+      currCoords: [longitude, latitude]
+    });
+  }
+  
+  onChangeInputHandler(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
+
+  addMarker(longitude, latitude) {
     let newMarkers = this.state.markers;
     newMarkers.push([longitude, latitude]);
-    console.log(`newMarkers: ${JSON.stringify(newMarkers)}`);
-    console.log(`this.state.markers: ${JSON.stringify(this.state.markers)}`);
     this.setState({ markers: newMarkers });
   }
 
+  removeInput(e) {
+    e.preventDefault();
+    console.log(`adding marker and removing input`)
+    this.addMarker(this.state.currCoords[0], this.state.currCoords[1]);
+    $(e.target).remove();
+  }
+
   render() {
-    const {viewport} = this.state;
+    const { viewport } = this.state;
 
     return (
       <MapGL
@@ -40,16 +75,28 @@ class Map extends Component {
         mapStyle="mapbox://styles/mapbox/dark-v9"
         onViewportChange={v => this.setState({viewport: v})}
         mapboxApiAccessToken={config.MAPBOX_APP_TOKEN}
-        onClick={this.addMarker}
+        onClick={this.appendInput}
       >
-        {/* {this.state.markers.length ? 
-          this.state.markers.map((m, i) => 
-            <Marker coordinates={m} key={i} >
-              {`Clicked here: ${m.longitude}, ${m.latitude}`}
-            </Marker>
-          ) 
+        {this.state.inputForm.length !== 0 ?
+          this.state.inputForm.map((input, i) =>
+            <form onSubmit={this.removeInput}>
+              <InputElement type='text' key={i} onChange={this.onChangeInputHandler}/>
+            </form>
+          )
+        : null}
+
+        {this.state.markers.length !== 0 ? 
+          this.state.markers.map((m, i) => {
+            console.log(m, i)
+            return (
+              <Marker latitude={m[1]} longitude={m[0]} key={i} >
+                {`Clicked here: ${m[1]}, ${m[0]}`}
+              </Marker>
+            )
+          }
+          )
           : null
-        } */}
+        }
       </MapGL>
     );
   }
